ab15bd99de8428befb9f04cc29301e0537c008ce
holly <holly.gummersonn@gmail.com>
2025-01-10 22:57:14 -0800

    added different clothing types to upload and recommendation

--- weatherRanking.py
+++ weatherRanking.py
@@ -1,54 +1,44 @@
-
-# Basic Temperature
-
-# # ranges
-# REALLY_COLD = (-100, -5) # cold pant + cold shirt + coat
-# COLD = (-5, 10) # cold pant + cold shirt
-# MEDIUM = (10, 20) # cold pant + hot shirt
-# HOT = (20, 100)   # hot pant + hot shirt
-
 # Basic Conditions
 RAIN = "rain", "drizzle", "thunderstorm"
 WIND = "windy"
 
 # clothing !!todo edit the tagging options to be more specific!!
-COLD_PANTS = "pants", "pants"
-COLD_TOPS = "sweater", "shirt", "coat"
+COLD_PANTS = "pants", "jeans", "sweatpants"
+COLD_TOPS = "sweater", "longsleeve", "hoodie"
 COATS = "coat" ,"coat"
-HOT_PANTS = "shorts", "skirt", "dress",
-HOT_SHIRTS = "shirt", "shirt"
+HOT_PANTS = "shorts", "skirt", "dress"
+HOT_SHIRTS = "shirt", "tanktop", "polo"
 RAIN_CLOTH = "raincoat","raincoat"
 WIND_CLOTH = "rain coat", "coat"
 
+# returns the catergories to search for in the database based on the weather type
 def find_conditions(temp, rain, wind):
     # windy = is_windy(wind)
-    # rainy = is_raining(rain)
-
+    rainy = is_raining(rain)
     really_cold = is_reallycold(temp)
     cold = is_cold(temp)
     medium = is_medium(temp)
 
     if really_cold:
-        if is_raining(rain):
+        if rainy:
             return "really cold", (COLD_PANTS, COLD_TOPS, RAIN_CLOTH)
         else:
             return "really cold", (COLD_PANTS, COLD_TOPS)
     elif cold:
-        if is_raining(rain):
+        if rainy:
             return "cold", (COLD_PANTS, COLD_TOPS, RAIN_CLOTH)
         else:
             return "cold", (COLD_PANTS, COLD_TOPS)
     elif medium: 
-        if is_raining(rain):
+        if rainy:
             return "medium", (COLD_PANTS, HOT_SHIRTS, RAIN_CLOTH)
         else:
             return "medium", (COLD_PANTS, HOT_SHIRTS)
     else:
-        if is_raining(rain):
+        if rainy:
             return "hot", (HOT_PANTS, HOT_SHIRTS, RAIN_CLOTH)
         else:
             return "hot", (HOT_PANTS, HOT_SHIRTS)
-
 
 
 # methods to find out temp ranking
